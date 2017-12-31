31e24e0cca4c3249cbe11ebf193976b471bf5b44
jyoti _pareek <jyoti_pareek@neovasolutions.com>
2017-12-31 20:14:37 +0530

    Chnages in env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,12 +16,12 @@ Before do |scenario|
  @screenshot_dir="./DebugReport/screenshots"
 end
 
-#After do |scenario|
-#  if(scenario.failed)
-#    time=Time.now.strftime('%Y_%m_%d_%h_%m_%s')
-#   name_of_scenario=time+scenario.name
-#    file_name=File.expand_path(@screenshot_dir)+'/'+name_of_scenario+'.png'
-#      driver.save_screenshot file_name
-#  else
-#  end
-#end
+After do |scenario|
+  if(scenario.failed)
+    time=Time.now.strftime('%Y_%m_%d_%h_%m_%s')
+   name_of_scenario=time+scenario.name
+    file_name=File.expand_path(@screenshot_dir)+'/'+name_of_scenario+'.png'
+      driver.save_screenshot file_name
+  else
+  end
+end
